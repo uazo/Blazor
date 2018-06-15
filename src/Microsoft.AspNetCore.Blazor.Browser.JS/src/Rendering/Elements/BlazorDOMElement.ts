28dfb200bd88e9b82f573322f2d214a064f36624
Carmelo Messina <carmelo.messina@uazo.it>
2018-06-15 09:46:13 +0200

    Improve performance on applyAttribute

--- src/Microsoft.AspNetCore.Blazor.Browser.JS/src/Rendering/Elements/BlazorDOMElement.ts
+++ src/Microsoft.AspNetCore.Blazor.Browser.JS/src/Rendering/Elements/BlazorDOMElement.ts
@@ -1,9 +1,10 @@
-﻿import { BrowserRenderer } from '../BrowserRenderer';
+import { BrowserRenderer } from '../BrowserRenderer';
 import { renderTreeFrame, RenderTreeFramePointer } from '../RenderTreeFrame';
 import { getRegisteredCustomTag } from '../../Interop/RenderingFunction';
 import { BlazorINPUTElement } from './BlazorINPUTElement';
 
 const logicalBlazorDomElementPropname = createSymbolOrFallback('_blazorDomElement');
+const logicalBlazorChildElementPropname = createSymbolOrFallback('_blazorDomChild');
 //const logicalBlazorDomChildArrayPropname = createSymbolOrFallback('_blazorDomChildArrayPropname');
 
 export class BlazorDOMElement {
@@ -19,7 +20,8 @@ export class BlazorDOMElement {
 		this.startContainer = start;
 		this.endContainer = end;
 
-		this.startContainer[logicalBlazorDomElementPropname] = this;
+    this.startContainer[logicalBlazorDomElementPropname] = this;
+    this.startContainer[logicalBlazorChildElementPropname] = [];
 	}
 
 	protected isComponent(): boolean {
@@ -35,7 +37,10 @@ export class BlazorDOMElement {
 	}
 
 	public getLogicalChild(childIndex: number): Node | BlazorDOMElement | null {
-		let element: Node | null = this.startContainer;
+    let cachedChild = this.startContainer[logicalBlazorChildElementPropname][childIndex];
+    if (cachedChild !== undefined) return cachedChild;
+
+    let element: Node | null = this.startContainer;
 		if (this.isComponent() === false)
 			element = element.firstChild;
 		else
@@ -45,8 +50,9 @@ export class BlazorDOMElement {
 			// no child
 			return null;
 		}
-		else {
-			while (childIndex > 0) {
+    else {
+      let ci = childIndex;
+      while (ci > 0) {
 				// skip if is this.Range
 				if (element !== this.startContainer) {
 					// is a component ?
@@ -56,17 +62,19 @@ export class BlazorDOMElement {
 					}
 				}
 
-				childIndex--;
+        ci--;
 
 				element = element!.nextSibling;
 				if (element == null) return null;
 			}
 
 			let blazorDom = this.getComponentFromNode(element);
-			if (blazorDom != null) {
+      if (blazorDom != null) {
+        this.startContainer[logicalBlazorChildElementPropname][childIndex] = blazorDom;
 				return blazorDom;
 			}
 
+      this.startContainer[logicalBlazorChildElementPropname][childIndex] = element;
 			return element;
 		}
 	}
@@ -92,7 +100,9 @@ export class BlazorDOMElement {
 		else {
 			parentElement.insertBefore(node, realSibling as Node);
 		}
-	}
+
+    this.startContainer[logicalBlazorChildElementPropname] = [];
+  }
 
 	public removeFromDom(childIndex: number | null = null) {
 		if (childIndex === null) {
@@ -103,7 +113,7 @@ export class BlazorDOMElement {
 
 			// Clear whole range
 			range.deleteContents();
-			range.detach();
+      range.detach();
 		}
 		else {
 			const element = this.getLogicalChild(childIndex)!;
@@ -114,8 +124,9 @@ export class BlazorDOMElement {
 			else {
 				// Remove only the childindex-nth element
 				element.parentElement!.removeChild(element as Node);
-			}
+      }
 		}
+    this.startContainer[logicalBlazorChildElementPropname] = [];
 	}
 
 	public updateText(childIndex: number, newText: string | null) {
@@ -126,19 +137,22 @@ export class BlazorDOMElement {
 	public applyAttribute(componentId: number, attributeFrame: RenderTreeFramePointer) {
 		//const toDomElement = this.Range.startContainer as Element;
 		//const browserRendererId = this.browserRenderer.browserRendererId;
-		const attributeName = renderTreeFrame.attributeName(attributeFrame)!;
-		const attributeValue = renderTreeFrame.attributeValue(attributeFrame);
+    const attributeName = renderTreeFrame.attributeName(attributeFrame)!;
+    if (this.isDOMAttributeEvent(attributeName)) {
+      const eventHandlerId = renderTreeFrame.attributeEventHandlerId(attributeFrame);
+      if (this.applyEvent(attributeName, componentId, eventHandlerId) == true) {
+        return;
+      }
+    }
 
+		const attributeValue = renderTreeFrame.attributeValue(attributeFrame);
 		if (this.isDOMAttribute(attributeName, attributeValue) == false) {
 			return; // If this DOM element type has special 'value' handling, don't also write it as an attribute
 		}
 
-		const eventHandlerId = renderTreeFrame.attributeEventHandlerId(attributeFrame);
-		if (this.applyEvent(attributeName, componentId, eventHandlerId) == false) {
-			// Treat as a regular string-valued attribute
-			this.setAttribute(attributeName, attributeValue);
-		}
-	}
+    // Treat as a regular string-valued attribute
+    this.setAttribute(attributeName, attributeValue);
+  }
 
 	public removeAttribute(childIndex: number, attributeName: string) {
 		// maybe must be rewritten (never go inside for now)
@@ -160,7 +174,14 @@ export class BlazorDOMElement {
 	protected isDOMAttribute(attributeName: string, value: string | null): boolean {
 		// default is true
 		return true;
-	}
+  }
+
+  protected isDOMAttributeEvent(attributeName: string): boolean {
+    const firstTwoChars = attributeName.substring(0, 2);
+    if (firstTwoChars === 'on') return true;
+
+    return false;
+  }
 
 	protected applyEvent(attributeName: string, componentId: number, eventHandlerId: number): boolean {
 		const toDomElement = this.startContainer as Element;
